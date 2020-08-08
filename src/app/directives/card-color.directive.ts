22a919f1651f9ec33cb9d8e950d6b42069c9320c
Alexander Fraltsov <kolonessens@yandex.ru>
2020-08-08 23:05:26 +0400

    feat: added hostBinding to directive

--- src/app/directives/card-color.directive.ts
+++ src/app/directives/card-color.directive.ts
@@ -1,13 +1,15 @@
-import { Directive, ElementRef, Renderer2, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, Input, OnInit, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appCardColor]'
 })
 export class CardColorDirective implements OnInit {
   @Input() public appCardColor: string;
+  @HostBinding('style.border-color') public borderColor: string = 'yellow';
+
   constructor(private el: ElementRef, private r: Renderer2) {}
 
   public ngOnInit(): void {
-    this.r.setStyle(this.el.nativeElement, 'border-color', this.appCardColor);
+    this.borderColor = this.appCardColor;
   }
 }
