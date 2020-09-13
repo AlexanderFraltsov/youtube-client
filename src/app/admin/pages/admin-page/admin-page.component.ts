0b028703d8d44043779e90c19b146c0deddac7cd
Alexander Fraltsov <kolonessens@yandex.ru>
2020-09-13 15:41:03 +0400

    feat: admin page layout is implemented

--- src/app/admin/pages/admin-page/admin-page.component.ts
+++ src/app/admin/pages/admin-page/admin-page.component.ts
@@ -1,3 +1,5 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-page.component.scss']
 })
 export class AdminPageComponent implements OnInit {
-
+  public adminForm: FormGroup;
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.adminForm = new FormGroup({
+      title: new FormControl('', [
+        Validators.required
+      ]),
+      description: new FormControl('', [
+        Validators.required,
+      ]),
+      image: new FormControl('', [
+        Validators.required
+      ]),
+      link: new FormControl('', [
+        Validators.required
+      ])
+    });
   }
 
+  public submit(): void {
+    console.log({...this.adminForm.value, publishedAt: new Date().toUTCString()});
+  }
 }
