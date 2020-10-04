4f641a8667a631a48b2ea970fbed9790fde67e5d
Alexander Fraltsov <kolonessens@yandex.ru>
2020-10-04 22:32:23 +0400

    fix: set some methods private at sort-cards pipe

--- src/app/youtube/pipes/sort-cards.pipe.ts
+++ src/app/youtube/pipes/sort-cards.pipe.ts
@@ -6,11 +6,10 @@ import { ISortOptions } from '../models/sort-options.model';
   name: 'sortCards'
 })
 export class SortCardsPipe implements PipeTransform {
-
-  public getDate: (item: ISearchItem) => number =
+  private getDate: (item: ISearchItem) => number =
     (item: ISearchItem): number => Date.parse(item.snippet.publishedAt)
 
-  public getViews: (item: ISearchItem) => number =
+  private getViews: (item: ISearchItem) => number =
     (item: ISearchItem): number => +item.statistics.viewCount
 
   public transform(items: ISearchItem[], sortOptions: ISortOptions): ISearchItem[] {
@@ -19,10 +18,10 @@ export class SortCardsPipe implements PipeTransform {
     if (sortOptions === null) { return items; }
     const directionModificator: 1 | -1 = (sortOptions.sortDirection === 'up') ? 1 : -1;
     const func: ( item: ISearchItem ) => number =
-      (sortOptions.sort === 'date') ? this.getDate : this.getViews;
+    (sortOptions.sort === 'date') ? this.getDate : this.getViews;
 
-    return [...items].sort((el1, el2) => {
-      return (func(el1) - func(el2)) * directionModificator;
-    });
+    return [...items].sort(
+      (el1, el2) => (func(el1) - func(el2)) * directionModificator
+    );
   }
 }
